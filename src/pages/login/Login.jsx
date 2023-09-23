04d4b4ff332e98d9f1514d31d624268f4012ba10
dgagan2 <andgag@duck.com>
2023-09-23 13:28:18 -0500

    Se agrega funcionalidad el formulario de login con validadores y handleSubmit y handleChange

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
-import logo from '../../assets/logo.png'
+import React, { useState, useEffect } from 'react'
+
 import './login.css'
 import { Link } from 'react-router-dom'
+import Logo from '../../components/Logo'
+import useForm from '../../hooks/useForms'
+import validateFormLogin from './validateFormLogIn'
+
 const Login = () => {
+  const [errorsLogin, setErrorsLogin] = useState({})
+  const sendData = () => {
+    const newErros = validateFormLogin(input)
+    setErrorsLogin(newErros)
+  }
+  const { input, handleSubmit, handleInputChange } = useForm(sendData, {
+    email: '',
+    password: ''
+  }
+  )
+
   return (
     <>
       <main className='login'>
         <article className='container-login'>
-          <div className='container-logo'>
-            <img src={logo} alt='' />
-            <h1>Oura Movie</h1>
-          </div>
-          <form action='' className='form-floating'>
+          <Logo />
+          <form className='form-floating' onSubmit={handleSubmit}>
 
             <h2 className='text-center'>Inicia Sesión</h2>
             <div className='form-login d-flex flex-column gap-2'>
-              <label htmlFor=''>Usuario</label>
-              <input type='text' className='form-control' />
-              <label htmlFor=''>Contraseña</label>
-              <input type='password' className='form-control' />
+              <label htmlFor='email'>Usuario</label>
+              <input type='text' id='email' name='email' className='form-control' value={input.email} onChange={handleInputChange} />
+              {errorsLogin.email && <p className='errores'>{errorsLogin.email}</p>}
+              <label htmlFor='password'>Contraseña</label>
+              <input type='password' id='password' name='password' className='form-control' value={input.password} onChange={handleInputChange} />
+              {errorsLogin.password && <span className='errores'>{errorsLogin.password}</span>}
             </div>
             <div className='container-button-login'>
               <button className='w-100 btn btn-lg btn-primary'>Iniciar sesión</button>
@@ -27,6 +41,7 @@ const Login = () => {
           </form>
           <b>¿Primera vez en Oura Movie?</b>
           <Link to='/account/registration'>Regístrese</Link>
+          <Link to='/account/forgotpassword'>¿Olvidaste tu contraseña?</Link>
         </article>
       </main>
     </>
